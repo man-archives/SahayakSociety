61ab7a0489beda0e2a662873208d45e3e5ee5ac0
Man Parvesh <manparveshsinghrandhawa@gmail.com>
2015-06-11 21:22:30 +0530

    Adding first block of code for Android App. Working basic UI + main screen.

---
+++ Application_Code/SahayakSociety/app/src/main/java/sahayaksociety/summer15/manparvesh/sahayaksociety/Splash.java
@@ -0,0 +1,63 @@
+package sahayaksociety.summer15.manparvesh.sahayaksociety;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Splash extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread logoTimer = new Thread() {
+            public void run(){
+                try{
+                    int logoTimer = 0;
+                    while(logoTimer < 2000){
+                        sleep(100);
+                        logoTimer = logoTimer +100;
+                    };
+                    startActivity(new Intent(getApplicationContext(),Login.class));
+                }
+
+                catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                finally{
+                    finish();
+                }
+            }
+        };
+
+        logoTimer.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
